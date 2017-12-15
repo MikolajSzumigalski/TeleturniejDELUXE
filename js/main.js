c28336f8b5e03c4d95142458d3e36f58c988299a
MkolajSzumigalski <m.szumigalski@op.pl>
2017-12-15 20:15:40 +0100

    przelicznik punktów

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
-/*if (sessionStorage.clickcount) {
-    sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
-} else {
-    sessionStorage.clickcount = 1;
-}*/
-//document.getElementById("t1pkt").innerHTML = sessionStorage.clickcount;
+odswierz();
 function odswierz() {
     if (sessionStorage.count1)
         document.getElementById("t1pkt").innerHTML = sessionStorage.count1;
     else {
         sessionStorage.count1 = 0;
+        document.getElementById("t1pkt").value = sessionStorage.count1;
     }
     if (sessionStorage.count2)
         document.getElementById("t2pkt").innerHTML = sessionStorage.count2;
     else {
         sessionStorage.count2 = 0;
+        document.getElementById("t2pkt").value = sessionStorage.count2;
     }
     if (sessionStorage.count3)
         document.getElementById("t3pkt").innerHTML = sessionStorage.count3;
     else {
         sessionStorage.count3 = 0;
+        document.getElementById("t3pkt").value = sessionStorage.count3;
     }
     if (sessionStorage.count4)
         document.getElementById("t4pkt").innerHTML = sessionStorage.count4;
     else {
         sessionStorage.count4 = 0;
+        document.getElementById("t4pkt").value = sessionStorage.count4;
     }
 }
 
@@ -38,4 +37,12 @@ document.getElementById("btnadd").addEventListener("click", function () {
     if (document.getElementById("team").value == "4")
         sessionStorage.count4 = Number(sessionStorage.count4) + Number(document.getElementById("points").value);
     odswierz();
+})
+
+document.getElementById("btnpowrot2").addEventListener("click", function () {
+    console.log("dupa");
+    sessionStorage.count1 = (Number(sessionStorage.count1)-1000)/100;
+    sessionStorage.count2 = (Number(sessionStorage.count2)-1000)/100;
+    sessionStorage.count3 = (Number(sessionStorage.count3)-1000)/100;
+    sessionStorage.count4 = (Number(sessionStorage.count4)-1000)/100;
 })
