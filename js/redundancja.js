0368c8594c368b8867b595c3c9008b6e3523b551
Stanislaw-Golebiewski <slowik66@o2.pl>
2017-12-16 20:49:12 +0100

    spakowano tworzenie pytań w funkcję

--- js/redundancja.js
+++ js/redundancja.js
@@ -1,412 +1,154 @@
-document.getElementById("q21").addEventListener("click", function(){
-                $("#pq21").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq21").css('z-index', 1);
+function brak_redundancji(row, column) {
+    document
+        .getElementById("q" + row + column)
+        .addEventListener("click", function() {
+            var div_id = "#pq" + row + column,
+                btn_id = "#qbtn" + row + column,
+                answer_id = "#a" + row + column,
+                answer_btn_id = "#abtn" + row + column,
+                q_id = "#q" + row + column;
 
-                $("#qbtn21").click(function () {
-                    $("#pq21").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq21").css('z-index', -1);
-                    });
-                    $("#q21").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
+            $(div_id).animate(
+                {
+                    opacity: "1",
+                },
+                "slow"
+            );
+            $(div_id).css("z-index", 1);
 
-                $("#abtn21").click(function () {
-                    $("#a21").animate({
-                        opacity: '1'
-                    }, "slow");
+            $(btn_id).click(function() {
+                $(div_id).animate(
+                    {
+                        opacity: "0",
+                    },
+                    "slow",
+                    function() {
+                        $(div_id).css("z-index", 0);
+                    }
+                );
+                $(q_id).css({
+                    "background-image": "none",
+                    "background-color": "black",
                 });
+            });
 
+            $(answer_btn_id).click(function() {
+                $(answer_id).animate(
+                    {
+                        opacity: "1",
+                    },
+                    "slow"
+                );
+            });
+        });
+}
+
+for (var i = 2; i < 5; i++) {
+    for (var j = 1; j < 5; j++) {
+        brak_redundancji(i, j);
+    }
+}
+
+document.getElementById("bbon11").addEventListener("click", function() {
+    console.log("dupa");
+    $("#bon11").animate(
+        {
+            opacity: "1",
+        },
+        "slow"
+    );
+    $("#bon11").css("z-index", 2);
+
+    $("#wbtn11").click(function() {
+        $("#bon11").animate(
+            {
+                opacity: "0",
+            },
+            "slow",
+            function() {
+                $("#bon11").css("z-index", -1);
+            }
+        );
+        $(".bbon11").css({
+            "background-image": "none",
+            "background-color": "black",
+        });
+    });
+
+    $("#abon11").click(function() {
+        $("#bona11").animate(
+            {
+                opacity: "1",
+            },
+            "slow"
+        );
+    });
 });
+document.getElementById("bbon12").addEventListener("click", function() {
+    $("#bon12").animate(
+        {
+            opacity: "1",
+        },
+        "slow"
+    );
+    $("#bon12").css("z-index", 2);
 
-document.getElementById("q22").addEventListener("click", function(){
-                $("#pq22").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq22").css('z-index', 1);
+    $("#wbtn12").click(function() {
+        $("#bon12").animate(
+            {
+                opacity: "0",
+            },
+            "slow",
+            function() {
+                $("#bon12").css("z-index", -1);
+            }
+        );
+        $("#bbon12").css({
+            "background-image": "none",
+            "background-color": "black",
+        });
+    });
 
-                $("#qbtn22").click(function () {
-                    $("#pq22").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq22").css('z-index', -1);
-                    });
-                    $("#q22").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn22").click(function () {
-                    $("#a22").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
+    $("#abon12").click(function() {
+        $("#bona12").animate(
+            {
+                opacity: "1",
+            },
+            "slow"
+        );
+    });
 });
+document.getElementById("bbon13").addEventListener("click", function() {
+    $("#bon13").animate(
+        {
+            opacity: "1",
+        },
+        "slow"
+    );
+    $("#bon13").css("z-index", 2);
 
-document.getElementById("q23").addEventListener("click", function(){
-                $("#pq23").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq23").css('z-index', 1);
+    $("#wbtn13").click(function() {
+        $("#bon13").animate(
+            {
+                opacity: "0",
+            },
+            "slow",
+            function() {
+                $("#bon13").css("z-index", -1);
+            }
+        );
+        $("#bbon13").css({
+            "background-image": "none",
+            "background-color": "black",
+        });
+    });
 
-                $("#qbtn23").click(function () {
-                    $("#pq23").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq23").css('z-index', -1);
-                    });
-                    $("#q23").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn23").click(function () {
-                    $("#a23").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q24").addEventListener("click", function(){
-                $("#pq24").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq24").css('z-index', 1);
-
-                $("#qbtn24").click(function () {
-                    $("#pq24").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq24").css('z-index', -1);
-                    });
-                    $("#q24").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn24").click(function () {
-                    $("#a24").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q31").addEventListener("click", function(){
-                $("#pq31").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq31").css('z-index', 1);
-
-                $("#qbtn31").click(function () {
-                    $("#pq31").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq31").css('z-index', -1);
-                    });
-                    $("#q31").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn31").click(function () {
-                    $("#a31").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q32").addEventListener("click", function(){
-                $("#pq32").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq32").css('z-index', 1);
-
-                $("#qbtn32").click(function () {
-                    $("#pq32").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq32").css('z-index', -1);
-                    });
-                    $("#q32").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn32").click(function () {
-                    $("#a32").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q33").addEventListener("click", function(){
-                $("#pq33").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq33").css('z-index', 1);
-
-                $("#qbtn33").click(function () {
-                    $("#pq33").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq33").css('z-index', -1);
-                    });
-                    $("#q33").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn33").click(function () {
-                    $("#a33").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q34").addEventListener("click", function(){
-                $("#pq34").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq34").css('z-index', 1);
-
-                $("#qbtn34").click(function () {
-                    $("#pq34").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq34").css('z-index', -1);
-                    });
-                    $("#q34").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn34").click(function () {
-                    $("#a34").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q41").addEventListener("click", function(){
-                $("#pq41").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq41").css('z-index', 1);
-
-                $("#qbtn41").click(function () {
-                    $("#pq41").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq41").css('z-index', -1);
-                    });
-                    $("#q41").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn41").click(function () {
-                    $("#a41").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-document.getElementById("q42").addEventListener("click", function(){
-                $("#pq42").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq42").css('z-index', 1);
-
-                $("#qbtn42").click(function () {
-                    $("#pq42").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq42").css('z-index', -1);
-                    });
-                    $("#q42").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn42").click(function () {
-                    $("#a42").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q43").addEventListener("click", function(){
-                $("#pq43").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq43").css('z-index', 1);
-
-                $("#qbtn43").click(function () {
-                    $("#pq43").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq43").css('z-index', -1);
-                    });
-                    $("#q43").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn43").click(function () {
-                    $("#a43").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("q44").addEventListener("click", function(){
-                $("#pq44").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#pq44").css('z-index', 1);
-
-                $("#qbtn44").click(function () {
-                    $("#pq44").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#pq44").css('z-index', -1);
-                    });
-                    $("#q44").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abtn44").click(function () {
-                    $("#a44").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-
-document.getElementById("bbon11").addEventListener("click", function(){
-                $("#bon11").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#bon11").css('z-index', 2);
-
-                $("#wbtn11").click(function () {
-                    $("#bon11").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#bon11").css('z-index', -1);
-                    });
-                    $("#bbon11").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abon11").click(function () {
-                    $("#bona11").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-document.getElementById("bbon11").addEventListener("click", function(){
-                console.log("dupa");
-                $("#bon11").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#bon11").css('z-index', 2);
-
-                $("#wbtn11").click(function () {
-                    $("#bon11").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#bon11").css('z-index', -1);
-                    });
-                    $(".bbon11").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abon11").click(function () {
-                    $("#bona11").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-document.getElementById("bbon12").addEventListener("click", function(){
-                $("#bon12").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#bon12").css('z-index', 2);
-
-                $("#wbtn12").click(function () {
-                    $("#bon12").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#bon12").css('z-index', -1);
-                    });
-                    $("#bbon12").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abon12").click(function () {
-                    $("#bona12").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
-});
-document.getElementById("bbon13").addEventListener("click", function(){
-                $("#bon13").animate({
-                    opacity: '1'
-                }, "slow");
-                $("#bon13").css('z-index', 2);
-
-                $("#wbtn13").click(function () {
-                    $("#bon13").animate({
-                        opacity: '0'
-                    }, "slow", function () {
-                        $("#bon13").css('z-index', -1);
-                    });
-                    $("#bbon13").css({
-                        "background-image": "none",
-                        "background-color": "black"
-                    });
-                })
-
-                $("#abon13").click(function () {
-                    $("#bona13").animate({
-                        opacity: '1'
-                    }, "slow");
-                });
-
+    $("#abon13").click(function() {
+        $("#bona13").animate(
+            {
+                opacity: "1",
+            },
+            "slow"
+        );
+    });
 });
