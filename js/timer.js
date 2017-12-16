a55f8bd09a6ebdf3a63ea0e7dd0bba5f7057cee4
Stanislaw-Golebiewski <slowik66@o2.pl>
2017-12-16 22:44:33 +0100

    timer code

---
+++ js/timer.js
@@ -0,0 +1,22 @@
+function startTimer(duration, display) {
+    //duration - w sekundach, display - parent element w ktrym bedzie wyswietalny zegar
+    var timer = duration,
+        minutes,
+        seconds;
+    setInterval(function() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        if (minutes === 0 && seconds < 10) {
+            display.style.color = red;
+        }
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
